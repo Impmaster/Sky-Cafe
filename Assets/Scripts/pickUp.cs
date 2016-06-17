447a84ab9ada9510cc858b5c29835fb5adeb802d
Alix Ollivier <aollivier82@gmail.com>
2016-06-17 11:20:03 +0800

    Added a cursor

---
+++ Assets/Scripts/pickUp.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+//MADE BY ALIX OLLIVIER
+
+public class pickUp : MonoBehaviour {
+
+	public Camera cam;
+
+	public int distanceFromObject = 5;
+
+	//When the items Layer is created, it has to be changed here.
+	public int itemsLayer = 8;
+
+	// Use this for initialization
+	void Start () {
+			itemsLayer = 1 << itemsLayer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    RaycastHit hit;
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+		//Casts a ray only on the "item" layer
+        if (Physics.Raycast(ray, out hit, distanceFromObject, itemsLayer)) {
+			if (Input.GetKeyDown(KeyCode.E)) {
+				hit.transform.gameObject.SetActive(false);
+			}
+			
+		}
+	}
+
+
+
+}
