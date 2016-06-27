080a0515d20b744a20d7b09444df9d5716be174e
Alix Ollivier <aollivier82@gmail.com>
2016-06-27 22:43:40 +0800

    NPC's now sit down and wait until they get a drink.

--- Assets/Scripts/NPC/Sit.cs
+++ Assets/Scripts/NPC/Sit.cs
@@ -5,11 +5,19 @@ public class Sit : MonoBehaviour {
 
 	public GameObject coaster;
 
+	holdMug mugHolder;
+
+	public void SetMugHolder() {
+		mugHolder = coaster.GetComponent<holdMug>();
+	}
+
 
 	//If player has put down the drink
 	public bool GotDrink() {
-		
+		if (mugHolder.holding()) {
+			return true;
+		}
 
- 		return true;
+ 		else return false;
 	}
 }
