725acac6aa5c8040054f0b0214c3aed674e371b9
Alix Ollivier <aollivier82@gmail.com>
2016-06-28 00:17:51 +0800

    Started NPC spawner.

---
+++ Assets/Scripts/NPC/NPCSpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCSpawner : MonoBehaviour {
+
+	public GameObject[] npcs;
+	public float timeBetween = 15f;
+	private int currentNpc = 0;
+
+	// Use this for initialization
+	void Start () {
+		for (int x = 0; x < npcs.Length; x++) {
+			npcs[x].SetActive(false);
+		}
+		StartCoroutine(Spawn());
+	}
+
+	void Update() {
+		//StartCoroutine(Spawn());
+	}
+
+
+	IEnumerator Spawn() {
+		if (currentNpc < npcs.Length) {
+			npcs[currentNpc].SetActive(true);
+			currentNpc++;
+		}
+		 yield return new WaitForSeconds(timeBetween);
+		 StartCoroutine(Spawn());
+	}
+	
+	
+}
