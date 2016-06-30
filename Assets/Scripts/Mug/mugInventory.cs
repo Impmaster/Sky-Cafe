2fa0715b14882224693633913c972731fd7c2f15
Alix Ollivier <aollivier82@gmail.com>
2016-06-30 23:10:37 +0800

    NPCs now wait and drink and have a happiness index.

--- Assets/Scripts/Mug/mugInventory.cs
+++ Assets/Scripts/Mug/mugInventory.cs
@@ -6,6 +6,19 @@ public class mugInventory : MonoBehaviour {
 	public string[] ingredients;
 	public int numberOfIngredients = 4;
 
+	public float warmTime = 30f;
+	public float coldTime = 45f;
+
+	private float startTime;
+	public float timeSpent;
+	public bool isFull;
+
+	public string currState = "Hot";
+
+	public Material hotState;
+	public Material warmState;
+	public Material coldState;
+
 	private int currNum = 0;
 
 	void Start() {
@@ -16,6 +29,30 @@ public class mugInventory : MonoBehaviour {
 		if (currNum < numberOfIngredients) {
 			ingredients[currNum] = name;
 			currNum++;
+		}
+
+		if (currNum == numberOfIngredients) {
+			isFull = true;
+		}
+
+	}
+
+	void Update() {
+		if (isFull) {
+			if (startTime == 0) {
+				startTime = Time.time;
+			}
+
+			timeSpent = Time.time - startTime;
+
+			if (timeSpent > warmTime) {
+				currState = "Warm";
+				//gameObject.GetComponentInChildren<Renderer>().;
+			}
+
+			if (timeSpent > coldTime) {
+				currState = "Cold";
+			}
 		}
 
 	}
---
+++ Assets/Scripts/NPC/Drink.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drink : MonoBehaviour {
+
+	public GameObject coaster;
+
+	public OrderDrink order;
+	
+	public float drinkTime = 5;
+
+	holdMug mugHolder;
+
+	mugInventory mug;
+
+	public int happiness;
+
+	public bool finishedDrinking;
+	
+	IEnumerator wait() {
+		yield return new WaitForSeconds(drinkTime);
+
+		if (mug.currState == "Hot") {
+			happiness++;
+		} else if (mug.currState == "Warm") {
+			//Do nothing, it's fine.
+		} else if (mug.currState == "Cold") {
+			happiness--;
+		}
+
+		int mugCof = 0;
+		int mugCre = 0;
+		int mugMil = 0;
+		int mugSug = 0;
+
+		int myCof = 0;
+		int myCre = 0;
+		int myMil = 0;
+		int mySug = 0;
+
+		for (int x = 0; x < mug.ingredients.Length; x++) {
+			if (mug.ingredients[x] == "Coffee") {
+				mugCof++;
+			} else if (mug.ingredients[x] == "Cream") {
+				mugCre++;
+			} else if (mug.ingredients[x] == "Milk") {
+				mugMil++; 
+			} else if (mug.ingredients[x] == "Sugar") {
+				mugSug++;
+			}
+		}
+		for (int x = 0; x < order.ingredients.Length; x++) {
+			if (order.ingredients[x] == "Coffee") {
+				myCof++;
+			} else if (order.ingredients[x] == "Cream") {
+				myCre++;
+			} else if (order.ingredients[x] == "Milk") {
+				myMil++; 
+			} else if (order.ingredients[x] == "Sugar") {
+				mySug++;
+			}
+
+		}
+		//If it's the wrong order happiness goes down.
+		if (mugCof != myCof || mugCre != myCre || mugMil != myMil || mugSug != mySug) {
+			happiness--;
+		}
+
+		finishedDrinking = true;
+	}
+
+	void drink() {
+
+		mugHolder = coaster.GetComponent<holdMug>();
+		mug = mugHolder.mug.GetComponent<mugInventory>();
+
+		StartCoroutine(wait());
+		
+	}
+}
