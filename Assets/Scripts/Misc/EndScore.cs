a5f4d258942d4450d4d6b2cb2a386be8dc5793e2
Alix Ollivier <aollivier82@gmail.com>
2016-09-01 16:30:16 -0700

    no big bugs pt 2

---
+++ Assets/Scripts/Misc/EndScore.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EndScore : MonoBehaviour {
+
+	public Text text;
+
+	private string endScore;
+
+	
+
+	public void showScore(int score) {
+		if (score <= 50 ) {
+			endScore = "You had an unlucky day. Day Over.";
+		} else if (score <= 70) {
+			endScore = "You missed a few orders, but overall you're doing pretty well. Day Over.";
+		} else if (score <= 100) {
+			endScore = "What a great day! Day Over.";
+		} else {
+			endScore = "You were flawless out there. Day Over.";
+		}
+
+		text.text = endScore;	
+
+	}
+
+}
---
+++ Assets/Scripts/Misc/WaitForEnd.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaitForEnd : MonoBehaviour {
+
+	public float numberDone;
+
+	public float numNPCs = 16;
+
+	PlayMakerFSM playmakerFSM;
+
+	void Start() {
+		playmakerFSM = GetComponent<PlayMakerFSM>();
+	}
+
+	void OnTriggerEnter() {
+		numberDone++;
+	}
+
+	void Update() {
+		if (numNPCs == numberDone) {
+			playmakerFSM.SendEvent("End Game");
+		}
+	}
+}
