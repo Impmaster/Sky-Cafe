3eec6cb3bb38371152b712882b7609223b1fd484
Alix Ollivier <aollivier82@gmail.com>
2016-09-01 18:59:11 -0700

    Fixed Cursor issues.

---
+++ Assets/Scripts/Misc/DisableMouse.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableMouse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+
+	void OnApplicationFocus() {
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+
+
+
+}
